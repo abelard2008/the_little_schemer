9f810515f5fcd53bbe5060514743734b5a514281
彭成章 <pengcz@auto-smart.com>
2018-07-07 06:23:49 +0800

    set?

--- theLittleSchemer.rkt
+++ theLittleSchemer.rkt
@@ -75,3 +75,111 @@
     (+ n 1)))
 
 (add1 68)
+
+(define set?
+  (lambda (lat)
+    (cond
+      ((null? lat) #t)
+      (else
+       (cond
+         ((member? (car lat) (cdr lat)) #f)
+         (else (set? (cdr lat))))))))
+
+(set? '(a a))
+
+(define makeset
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((member? (car lat) (cdr lat)) (makeset (cdr lat)))
+      (else
+       (cons (car lat)
+             (makeset (cdr lat)))))))
+
+(makeset '(apple peach pear peach plum apple lemon peach))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else (cond
+              ((member? (car set1) set2) (subset? (cdr set1) set2))
+              (else #f))))))
+
+(subset? '(4 pounds of horseradish) '(four pounds chicken and 5 ounces horseradish))
+
+(subset? '(5 chicken wings) '(5 hambugers 2 pieces fried chicken and light duckling wings))
+
+
+(define subset1?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else
+       (and (member? (car set1) set2)
+            (subset? (cdr set1) set2))))))
+(subset1? '(4 pounds of horseradish) '(four pounds chicken and 5 ounces horseradish))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and
+     (subset1? set1 set2)
+     (subset1? set2 set1))))
+
+(eqset? '(a) '(b))
+(eqset? '(a) '(a))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #f)
+      (else
+       (or (member? (car set1) set2) (intersect? (cdr set1) set2))))))
+
+(intersect? '(stewed tomatoes and macaroni) '(macaroni and cheese))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2)
+       (cons (car set1) (intersect (cdr set1) set2)))
+      (else (intersect (cdr set1) set2)))))
+
+(intersect '(stewed tomatoes and macaroni) '(macaroni and cheese))
+
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2)
+       (union (cdr set1) set2))
+      (else
+       (cons (car set1)
+             (union (cdr set1) set2))))))
+(union '(stewed tomatoes and macaroni) '(macaroni and cheese))
+
+(define complement
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2)
+       (complement (cdr set1) set2))
+      (else
+       (cons (car set1)
+             (complement (cdr set1) set2))))))
+
+(complement '(stewed tomatoes and macaroni) '(macaroni and cheese))
+
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+       (else
+        (intersect (car l-set)
+                   (intersectall (cdr l-set)))))))
+
+(intersectall '((6 pears and)
+                (3 peaches and 6 peppers)
+                (8 pears and 6 plums)
+                (and 6 prunes with some apples)))
